018c55520f04dd7f3c17176948bfb51b0a0ebbcf
dmihmih <dmihmih@gmail.com>
2020-07-22 13:52:49 +0300

    Changed development team and identifiers

--- Core/CoreTests/Use Cases/UploadManagerTests.swift
+++ Core/CoreTests/Use Cases/UploadManagerTests.swift
@@ -66,15 +66,15 @@ class UploadManagerTests: CoreTestCase {
     func testUploadURLSharedContainer() throws {
         UUID.mock("shared")
         let expected = URL
-            .sharedContainer("group.com.wiley.instructure.icanvas")?
+            .sharedContainer("group.com.wiley.icanvas.wel")?
             .appendingPathComponent("uploads/shared/")
             .appendingPathComponent(url.lastPathComponent)
         let config = URLSessionConfiguration.background(withIdentifier: "doesnt matter")
-        config.sharedContainerIdentifier = "group.com.wiley.instructure.icanvas"
+        config.sharedContainerIdentifier = "group.com.wiley.icanvas.wel"
         URLSessionAPI.delegateURLSession = { (configuration: URLSessionConfiguration, delegate: URLSessionDelegate?, delegateQueue: OperationQueue?) -> URLSession in
             return URLSession(configuration: config, delegate: delegate, delegateQueue: delegateQueue)
         }
-        let manager = UploadManager(identifier: "test", sharedContainerIdentifier: "group.com.wiley.instructure.icanvas")
+        let manager = UploadManager(identifier: "test", sharedContainerIdentifier: "group.com.wiley.icanvas.wel")
         XCTAssertEqual(try manager.uploadURL(url), expected)
     }
 
